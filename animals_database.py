34620084daac723ce6e7106c79a868d0808f0f99
hoanghieu.dang@gmail.com <hoanghieu.dang@gmail.com>
2024-11-09 14:14:53 +0100

    break down serialisation task of entire animal data to serialisation of single animal by defining new function. Using unordered list and use list element for each information of the animal instead of using <br/> to break line

--- animals_database.py
+++ animals_database.py
@@ -5,34 +5,45 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-
-def animals_list_display(file_path):
-    """reading the content of the json file
-       serialize the list of animals and their following information
-       into html list element:
+def single_animal_html_serialize(animal):
+    """
+    serialize the data of one single animal from animal data object
+    put the following information into a html list element:
        - Name
        - Diet
        - The first location from the locations list
        - Type
        if one of these fields doesn't exist, don't print it
     """
-    animals_data = load_data(file_path)
     animals_str_for_display = ""
-    for animal in animals_data:
-        animals_str_for_display += "<li class='cards__item'>"
-        if "name" in animal:
-            animals_str_for_display += f"name: {animal['name']}<br/>\n"
-        if "diet" in animal["characteristics"]:
-            animals_str_for_display += f"diet: {animal['characteristics']['diet']}<br/>"
-        if "type" in animal["characteristics"]:
-            animals_str_for_display += f"type: {animal['characteristics']['type']}<br/>"
-        if "locations" in animal:
-            if animal["locations"]:
-                animals_str_for_display += f"location: {animal['locations'][0]}<br/>"
-        animals_str_for_display += "</li>"
-
+    animals_str_for_display += "<li class='cards__item'>"
+    if "name" in animal:
+        animals_str_for_display += f"<div class='card__title'>{animal['name']}</div>"
+    animals_str_for_display += "<p class='card__text'>"
+    animals_str_for_display += "<ul class='card__text__ul'>"
+    if "diet" in animal["characteristics"]:
+        animals_str_for_display += f"<li class='ul__list__item'>Diet: {animal['characteristics']['diet']}</li>"
+    if "type" in animal["characteristics"]:
+        animals_str_for_display += f"<li class='ul__list__item'>Type: {animal['characteristics']['type']}</li>"
+    if "locations" in animal:
+        if animal["locations"]:
+            animals_str_for_display += f"<li class='ul__list__item'>location: {animal['locations'][0]}</li>"
+    animals_str_for_display += "</ul>"
+    animals_str_for_display += "</p>"
+    animals_str_for_display += "</li>"
     return animals_str_for_display
 
+def animals_html_list(file_path):
+    """
+    reading the content of the json file
+    iterate through the animal data
+    return the list of animals and their information as HTML list elements
+    """
+    animals_html_list = ""
+    animals_data = load_data(file_path)
+    for animal in animals_data:
+        animals_html_list += single_animal_html_serialize(animal)
+    return animals_html_list
 
 def animals_list_to_HTML_file(file_path, animals_data_str):
     """
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import animals_database as anim
 
 def main():
-    animals_list = anim.animals_list_display('animals_data.json')
-    #print(animals_list)
+    animals_list = anim.animals_html_list('animals_data.json')
     anim.animals_list_to_HTML_file('animals_template.html', animals_list)
 
 if __name__ == "__main__":
